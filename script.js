08b0d1c59d189923146aa98f1c1b745e56a656b0
dziduistkrieg <48360975+dziduistkrieg@users.noreply.github.com>
2023-03-25 17:01:23 +0100

    add functionality for adding and deleting books

--- script.js
+++ script.js
@@ -10,10 +10,6 @@ function Book(title, author, numberOfPages, status) {
   };
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-}
-
 const lotr = new Book('Lord of the Rings', 'J.R.R.Tolkien', 295, 'read');
 const got = new Book('A Game of Thrones', 'George R.R. Martin', 694, 'read');
 const nineteen84 = new Book('1984', 'George Orwell', 328, 'unread');
@@ -22,8 +18,34 @@ myLibrary.push(lotr);
 myLibrary.push(got);
 myLibrary.push(nineteen84);
 
+function addBookToLibrary() {
+  const title = document.querySelector('input#title').value;
+  const author = document.querySelector('input#author').value;
+  const numberOfPages = document.querySelector('input#pages').value;
+  let status = 'unread';
+  if (document.querySelector('input#read').checked === true) {
+    status = 'read';
+  }
+  const book = new Book(title, author, numberOfPages, status);
+  myLibrary.push(book);
+  displayBook(book);
+  document.querySelector('input#title').value = '';
+  document.querySelector('input#author').value = '';
+  document.querySelector('input#pages').value = '';
+  document.querySelector('input#read').checked = false;
+}
+const submitBookButton = document.querySelector('form > button');
+submitBookButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+  formContainer.classList.remove('show');
+  mainContainer.classList.remove('active');
+});
+
 const cardContainer = document.querySelector('.card-container');
-myLibrary.forEach((book) => {
+
+function displayBook(book) {
+  console.log(book);
   const card = document.createElement('div');
   card.classList.add('card');
   card.setAttribute('data-index', `${myLibrary.indexOf(book)}`);
@@ -48,7 +70,14 @@ myLibrary.forEach((book) => {
   if (book.status === 'read') {
     btn.setAttribute('checked', 'true');
   }
-});
+  btn.addEventListener('click', changeStatus);
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'delete';
+  card.appendChild(deleteButton);
+  deleteButton.setAttribute('data-index', `${myLibrary.indexOf(book)}`);
+  deleteButton.addEventListener('click', deleteBook);
+}
+myLibrary.forEach((book) => displayBook(book));
 
 function changeStatus() {
   const index = this.attributes['data-index'].value;
@@ -60,6 +89,13 @@ function changeStatus() {
     status.textContent = 'read';
     myLibrary[index].status = 'read';
   }
+}
+
+function deleteBook() {
+  const index = this.attributes['data-index'].value;
+  const card = document.querySelector(`.card[data-index='${index}'`);
+  cardContainer.removeChild(card);
+  delete myLibrary[index];
 }
 
 const markButtons = document.querySelectorAll('.card input');
